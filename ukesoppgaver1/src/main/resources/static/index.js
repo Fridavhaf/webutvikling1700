33247534b07e086ae1cfb3e55cc282d970ccee16
Fridavhaf <frhaf7974@oslomet.no>
2024-01-24 12:55:41 +0100

    tasksVideo1

--- ukesoppgaver1/src/main/resources/static/index.js
+++ ukesoppgaver1/src/main/resources/static/index.js
@@ -79,8 +79,126 @@ Endre oppgave 9 ved å legge til en melding til console slik at den viser
 "Du har trykket på knappen X ganger" (hvor X er antall ganger knappen ble trykket på).*/
 //Two ways; first fancy, new way:
 let clicksWithString = 0;
-function nrOfClicksWithString(){
+function nrOfClicksWithString() {
     clicksWithString++;
     console.log(`This is the fancy version: ${clicksWithString}`);
     console.log("This is the not so fancy version: " + clicksWithString);
 }
+
+/*Oppgave 1
+Lag en HTML-knapp som kaller en JavaScript-funksjon som viser
+"Hei verden" på en blank side via "document.write".
+Bruk hendelsen "onClick" på knappen*/
+function hey(){
+    document.write("Hello world!");
+}
+
+/*Oppgave 2
+Gjør om oppgave 1 til å vise en "alert"-boks når knappen trykkes.*/
+function importantWelcomeMessage(){
+    alert("Hello world!");
+}
+
+/*Oppgave 3
+Lag et input-felt i en HTML-side. Lag et JavaScript som leser dataene fra
+input-feltet når det er endringer i feltet (hendelsen "onChange") og som
+skriver disse dataene ut på en blank side med "document.write". Funksjonen som
+kalles ved "onChange" skal ta en inn-parameter, som er verdien av input-feltet.*/
+
+function pageWithInput(newPageInput){
+    document.write(newPageInput);
+}
+
+/*Oppgave 4
+Endre oppgave 3 ved å legge ut en "pop-up/alert"-boks som skal inneholde
+teksten fra input-feltet.*/
+function alertWithInput(alertInput){
+    alert(alertInput);
+}
+
+/*Oppgave 5
+Utvid oppgave 4 med å skrive informasjonen fra input-feltet til "console.log".
+Sjekk at dataene blir skrevet til loggen.*/
+function consoleWithInput(consoleInput){
+    console.log(consoleInput);
+}
+
+/**Ekstraoppgaver**/
+/*Oppgave 6
+Utvid oppgave 3 ved å gi et farge til teksten som vises på blank side.*/
+function newPageColor(colorInput){
+    document.write(colorInput);
+    document.body.style.color = "red";
+    document.body.style.background="pink"
+}
+
+/*Oppgave 7
+Utvid oppgave 4 ved å vise input i alert boks i store bokstaver.*/
+function makeTextUpper(inputToUpper){
+    alert(inputToUpper.toUpperCase());
+}
+
+/*Oppgave 8
+Lag et knapp som skriver tall “1” til console (bruk console.log)
+hver gang den trykkes.*/
+function one(){
+    console.log(1);
+}
+
+/*Oppgave 9
+Utvid oppgave 8 slik at den skriver til console antall ganger
+knapp ble trykket. (Tell opp fra 1)*/
+let clicking = 0;
+function numberOfClicks(){
+    clicking++;
+    console.log(clicking);
+}
+
+/*Oppgave 10
+Endre oppgave 9 ved å legge til en melding til console slik at den viser
+"Du har trykket på knappen X ganger" (hvor X er antall ganger knappen ble
+trykket på).*/
+let clickings = 0;
+function numberOfClicksWithMessage(){
+    clickings++;
+    console.log("You have pressed the button " + clickings + " times");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
